8fdd27823b7db4f03f2cc0a6ef92cad59b9bfb5b
PTYin <PeterYin1604@gmail.com>
2019-12-14 21:38:09 +0800

    update README.md & optimize code

---
+++ README.md
@@ -0,0 +1,2 @@
+# CTW Baseline SSD Model Using Tensorflow2.0
+CTW(A Large Chinese Text Dataset in the Wild) Baseline SSD Model Using Tensorflow2.0
--- model.py
+++ model.py
@@ -1,115 +1,66 @@
-from import_keras import Input, Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, Flatten, BatchNormalization, Dense, Reshape, concatenate, K, Activation, Model
+from import_keras import Input, Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, Flatten, \
+    BatchNormalization, Dense, Reshape, concatenate, K, Activation, Model
 import tensorflow as tf
 from toolkit import PriorBox, Normalize
 
 
-
 def ssd(input_shape, num_classes):
-
     net = {}
     # Block 1
     input_tensor = Input(shape=input_shape)
     img_size = (input_shape[1], input_shape[0])
     net['input'] = input_tensor
-    net['conv1_1'] = Conv2D(64, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv1_1')(net['input'])
-    net['conv1_2'] = Conv2D(64, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv1_2')(net['conv1_1'])
-    net['pool1'] = MaxPooling2D((2, 2), strides=(2, 2), padding='same',
-                                name='pool1')(net['conv1_2'])
+    net['conv1_1'] = Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same', name='conv1_1')(net['input'])
+    net['conv1_2'] = Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same', name='conv1_2')(net['conv1_1'])
+    net['pool1'] = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool1')(net['conv1_2'])
+
     # Block 2
-    net['conv2_1'] = Conv2D(128, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv2_1')(net['pool1'])
-    net['conv2_2'] = Conv2D(128, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv2_2')(net['conv2_1'])
-    net['pool2'] = MaxPooling2D((2, 2), strides=(2, 2), padding='same',
-                                name='pool2')(net['conv2_2'])
+    net['conv2_1'] = Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same', name='conv2_1')(net['pool1'])
+    net['conv2_2'] = Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same', name='conv2_2')(net['conv2_1'])
+    net['pool2'] = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool2')(net['conv2_2'])
+
     # Block 3
-    net['conv3_1'] = Conv2D(256, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv3_1')(net['pool2'])
-    net['conv3_2'] = Conv2D(256, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv3_2')(net['conv3_1'])
-    net['conv3_3'] = Conv2D(256, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv3_3')(net['conv3_2'])
-    net['pool3'] = MaxPooling2D((2, 2), strides=(2, 2), padding='same',
-                                name='pool3')(net['conv3_3'])
+    net['conv3_1'] = Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same', name='conv3_1')(net['pool2'])
+    net['conv3_2'] = Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same', name='conv3_2')(net['conv3_1'])
+    net['conv3_3'] = Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same', name='conv3_3')(net['conv3_2'])
+    net['pool3'] = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool3')(net['conv3_3'])
+
     # Block 4
-    net['conv4_1'] = Conv2D(512, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv4_1')(net['pool3'])
-    net['conv4_2'] = Conv2D(512, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv4_2')(net['conv4_1'])
-    net['conv4_3'] = Conv2D(512, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv4_3')(net['conv4_2'])
-    net['pool4'] = MaxPooling2D((2, 2), strides=(2, 2), padding='same',
-                                name='pool4')(net['conv4_3'])
+    net['conv4_1'] = Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', name='conv4_1')(net['pool3'])
+    net['conv4_2'] = Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', name='conv4_2')(net['conv4_1'])
+    net['conv4_3'] = Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', name='conv4_3')(net['conv4_2'])
+    net['pool4'] = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='pool4')(net['conv4_3'])
+
     # Block 5
-    net['conv5_1'] = Conv2D(512, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv5_1')(net['pool4'])
-    net['conv5_2'] = Conv2D(512, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv5_2')(net['conv5_1'])
-    net['conv5_3'] = Conv2D(512, kernel_size=(3, 3),
-                            activation='relu',
-                            padding='same',
-                            name='conv5_3')(net['conv5_2'])
-    net['pool5'] = MaxPooling2D((3, 3), strides=(1, 1), padding='same',
-                                name='pool5')(net['conv5_3'])
+    net['conv5_1'] = Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', name='conv5_1')(net['pool4'])
+    net['conv5_2'] = Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', name='conv5_2')(net['conv5_1'])
+    net['conv5_3'] = Conv2D(512, kernel_size=(3, 3), activation='relu', padding='same', name='conv5_3')(net['conv5_2'])
+    net['pool5'] = MaxPooling2D((3, 3), strides=(1, 1), padding='same', name='pool5')(net['conv5_3'])
+
     # FC6
-    net['fc6'] = Conv2D(1024, kernel_size=(3, 3), dilation_rate=(6, 6),
-                        activation='relu', padding='same',
-                        name='fc6')(net['pool5'])
+    net['fc6'] = Conv2D(1024, kernel_size=(3, 3), dilation_rate=(6, 6), activation='relu', padding='same', name='fc6')(net['pool5'])
     # x = Dropout(0.5, name='drop6')(x)
+
     # FC7
-    net['fc7'] = Conv2D(1024, kernel_size=(1, 1), activation='relu',
-                        padding='same', name='fc7')(net['fc6'])
+    net['fc7'] = Conv2D(1024, kernel_size=(1, 1), activation='relu', padding='same', name='fc7')(net['fc6'])
     # x = Dropout(0.5, name='drop7')(x)
+
     # Block 6
-    net['conv6_1'] = Conv2D(256, kernel_size=(1, 1), activation='relu',
-                            padding='same',
-                            name='conv6_1')(net['fc7'])
-    net['conv6_2'] = Conv2D(512, kernel_size=(3, 3), strides=(2, 2),
-                            activation='relu', padding='same',
-                            name='conv6_2')(net['conv6_1'])
+    net['conv6_1'] = Conv2D(256, kernel_size=(1, 1), activation='relu', padding='same', name='conv6_1')(net['fc7'])
+    net['conv6_2'] = Conv2D(512, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same', name='conv6_2')(net['conv6_1'])
+
     # Block 7
-    net['conv7_1'] = Conv2D(128, kernel_size=(1, 1), activation='relu',
-                            padding='same',
-                            name='conv7_1')(net['conv6_2'])
+    net['conv7_1'] = Conv2D(128, kernel_size=(1, 1), activation='relu', padding='same', name='conv7_1')(net['conv6_2'])
     net['conv7_2'] = ZeroPadding2D()(net['conv7_1'])
-    net['conv7_2'] = Conv2D(256, kernel_size=(3, 3), strides=(2, 2),
-                            activation='relu', padding='valid',
-                            name='conv7_2')(net['conv7_2'])
+    net['conv7_2'] = Conv2D(256, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='valid', name='conv7_2')(net['conv7_2'])
+
     # Block 8
-    net['conv8_1'] = Conv2D(128, kernel_size=(1, 1), activation='relu',
-                            padding='same',
-                            name='conv8_1')(net['conv7_2'])
-    net['conv8_2'] = Conv2D(256, kernel_size=(3, 3), strides=(2, 2),
-                            activation='relu', padding='same',
-                            name='conv8_2')(net['conv8_1'])
+    net['conv8_1'] = Conv2D(128, kernel_size=(1, 1), activation='relu', padding='same', name='conv8_1')(net['conv7_2'])
+    net['conv8_2'] = Conv2D(256, kernel_size=(3, 3), strides=(2, 2), activation='relu', padding='same', name='conv8_2')(net['conv8_1'])
+
     # Last Pool
     net['pool6'] = GlobalAveragePooling2D(name='pool6')(net['conv8_2'])
+
     # Prediction from conv4_3
     net['conv4_3_norm'] = Normalize(20, name='conv4_3_norm')(net['conv4_3'])
     num_priors = 3
@@ -130,6 +81,7 @@ def ssd(input_shape, num_classes):
                         variances=[0.1, 0.1, 0.2, 0.2],
                         name='conv4_3_norm_mbox_priorbox')
     net['conv4_3_norm_mbox_priorbox'] = priorbox(net['conv4_3_norm'])
+
     # Prediction from fc7
     num_priors = 6
     net['fc7_mbox_loc'] = Conv2D(num_priors * 4, kernel_size=(3, 3),
@@ -149,6 +101,7 @@ def ssd(input_shape, num_classes):
                         variances=[0.1, 0.1, 0.2, 0.2],
                         name='fc7_mbox_priorbox')
     net['fc7_mbox_priorbox'] = priorbox(net['fc7'])
+
     # Prediction from conv6_2
     num_priors = 6
     x = Conv2D(num_priors * 4, kernel_size=(3, 3), padding='same',
@@ -168,6 +121,7 @@ def ssd(input_shape, num_classes):
                         variances=[0.1, 0.1, 0.2, 0.2],
                         name='conv6_2_mbox_priorbox')
     net['conv6_2_mbox_priorbox'] = priorbox(net['conv6_2'])
+
     # Prediction from conv7_2
     num_priors = 6
     x = Conv2D(num_priors * 4, kernel_size=(3, 3), padding='same',
@@ -187,6 +141,7 @@ def ssd(input_shape, num_classes):
                         variances=[0.1, 0.1, 0.2, 0.2],
                         name='conv7_2_mbox_priorbox')
     net['conv7_2_mbox_priorbox'] = priorbox(net['conv7_2'])
+
     # Prediction from conv8_2
     num_priors = 6
     x = Conv2D(num_priors * 4, kernel_size=(3, 3), padding='same',
@@ -206,6 +161,7 @@ def ssd(input_shape, num_classes):
                         variances=[0.1, 0.1, 0.2, 0.2],
                         name='conv8_2_mbox_priorbox')
     net['conv8_2_mbox_priorbox'] = priorbox(net['conv8_2'])
+
     # Prediction from pool6
     num_priors = 6
     x = Dense(num_priors * 4, name='pool6_mbox_loc_flat')(net['pool6'])
@@ -222,6 +178,7 @@ def ssd(input_shape, num_classes):
     net['pool6_reshaped'] = Reshape(target_shape,
                                     name='pool6_reshaped')(net['pool6'])
     net['pool6_mbox_priorbox'] = priorbox(net['pool6_reshaped'])
+
     # Gather all predictions
     net['mbox_loc'] = concatenate([net['conv4_3_norm_mbox_loc_flat'],
                                    net['fc7_mbox_loc_flat'],
